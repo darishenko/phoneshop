ce9bd962569ba7f5fec4021825fa52dec65e6ee6
DARYA <89320281+darishenko@users.noreply.github.com>
2023-08-31 17:37:55 +0300

    Added integration tests for PhoneDao and ColorDao

---
+++ core/src/test/java/com/es/core/model/color/JdbcColorDaoIntTest.java
@@ -0,0 +1,46 @@
+package com.es.core.model.color;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.annotation.Resource;
+import java.util.Set;
+
+import static org.testng.AssertJUnit.*;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/context/test-applicationContext-core.xml")
+public class JdbcColorDaoIntTest {
+    private static final String TABLE_PHONE_TO_COLOR = "phone2color";
+    private static final String ROW_PHONE_IN_TABLE_PHONE_TO_COLOR = "phoneId";
+    private static final Long PHONE_ID_WITH_COLORS = 1000L;
+    private static final Long PHONE_ID_WITHOUT_COLORS = 1002L;
+    @Resource
+    private ColorDao colorDao;
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void getPhoneColors_phoneWithColors_notEmptyColorSet() {
+        int expectedPhoneColorsCount = JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, TABLE_PHONE_TO_COLOR,
+                String.format("%s = %d", ROW_PHONE_IN_TABLE_PHONE_TO_COLOR, PHONE_ID_WITH_COLORS));
+
+        Set<Color> phoneColors = colorDao.getPhoneColors(PHONE_ID_WITH_COLORS);
+
+        assertFalse(phoneColors.isEmpty());
+        assertEquals(expectedPhoneColorsCount, phoneColors.size());
+    }
+
+    @Test
+    public void getPhoneColors_phoneWithoutColors_emptyColorSet() {
+        Set<Color> phoneColors = colorDao.getPhoneColors(PHONE_ID_WITHOUT_COLORS);
+
+        assertTrue(phoneColors.isEmpty());
+    }
+
+}
---
+++ core/src/test/java/com/es/core/model/phone/JdbcPhoneDaoIntTest.java
@@ -0,0 +1,81 @@
+package com.es.core.model.phone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.annotation.Resource;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/context/test-applicationContext-core.xml")
+public class JdbcPhoneDaoIntTest {
+    private static final String TABLE_PHONES = "PHONES";
+    private static final Long EXISTING_PHONE_ID = 1000L;
+    private static final Long NON_EXISTING_PHONE_ID = 0L;
+    @Resource
+    private PhoneDao phoneDao;
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void get_existingPhoneId_getProduct() {
+        Optional<Phone> phone = phoneDao.get(EXISTING_PHONE_ID);
+
+        assertTrue(phone.isPresent());
+        assertEquals(EXISTING_PHONE_ID, phone.get().getId());
+    }
+
+    @Test
+    public void get_nonExistingPhoneId_getPhone() {
+        Optional<Phone> phone = phoneDao.get(NON_EXISTING_PHONE_ID);
+
+        assertFalse(phone.isPresent());
+    }
+
+    @Test
+    public void save_newPhone_savePhone() {
+        int expectedPhonesCount = getPhonesTableRowCount();
+
+        phoneDao.save(createNewPhone("new brand", "new model"));
+
+        int phonesCount = getPhonesTableRowCount();
+        assertEquals(expectedPhonesCount + 1, phonesCount);
+    }
+
+    @Test
+    public void findAll_fullyPhonesTable_notEmptyResult() {
+        int expectedPhonesCount = getPhonesTableRowCount();
+
+        int phonesCount = phoneDao.findAll(0, expectedPhonesCount).size();
+
+        assertEquals(expectedPhonesCount, phonesCount);
+    }
+
+    @Test
+    public void findAll_emptyPhonesTable_emptyResult() {
+        int expectedPhonesCount = getPhonesTableRowCount();
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, TABLE_PHONES);
+
+        int phonesCount = phoneDao.findAll(0, expectedPhonesCount).size();
+
+        assertEquals(0, phonesCount);
+    }
+
+    private int getPhonesTableRowCount() {
+        return JdbcTestUtils.countRowsInTable(jdbcTemplate, TABLE_PHONES);
+    }
+
+    private Phone createNewPhone(String brand, String model) {
+        Phone phone = new Phone();
+        phone.setBrand(brand);
+        phone.setModel(model);
+        return phone;
+    }
+
+}
